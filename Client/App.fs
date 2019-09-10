8eb8534f7e38f01e135abc8cf6360eb624f7bbcf
Matheus Cruz <mlcruz@inf.ufrgs.br>
2019-09-09 22:36:42 -0300

    build events config

--- Client/App.fs
+++ Client/App.fs
@@ -1,17 +1,11 @@
 ﻿module App
 
-(**
- The famous Increment/Decrement ported from Elm.
- You can find more info about Elmish architecture and samples at https://elmish.github.io/
-*)
-
 open Elmish
 open Elmish.React
 open Fable.React
 open Fable.React.Props
 
 // MODEL
-
 type Model = int
 
 type Msg =
@@ -21,14 +15,12 @@ type Msg =
 let init() : Model = 0
 
 // UPDATE
-
 let update (msg:Msg) (model:Model) =
     match msg with
     | Increment -> model + 1
     | Decrement -> model - 1
 
 // VIEW (rendered with React)
-
 let view (model:Model) dispatch =
 
   div []
